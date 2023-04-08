f344a8096d5b6614c0c8b8c7c123ceb0ef320874
Vasin Igor <vasin.igor0796@gmail.net>
2023-04-08 15:46:03 +0300

    Basis

--- team_task.py
+++ team_task.py
@@ -1,1 +1,21 @@
 
+import random
+import string
+
+length = int(input("Введіть довжину паролю: "))
+uppercase = input("Включити великі літери (так/ні)? ").lower() == 'так'
+lowercase = input("Включити маленькі літери (так/ні)? ").lower() == 'так'
+numbers = input("Включити цифри (так/ні)? ").lower() == 'так'
+special = input("Включити знаки (так/ні)? ").lower() == 'так'
+
+# Створення списку доступних символів
+characters = ''
+if uppercase:
+    characters += string.ascii_uppercase
+if lowercase:
+    characters += string.ascii_lowercase
+if numbers:
+    characters += string.digits
+if special:
+    characters += string.punctuation
+    
